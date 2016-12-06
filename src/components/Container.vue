1af42dd300c51bfa181369c9ddc812cd08a4888d
Jeffrey Jose <jeffjosejeff@gmail.com>
2016-12-06 01:11:33 -0500

    adding the Container component

---
+++ src/components/Container.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script lang="coffee">
+
+module.exports =
+  name: 'container'
+
+</script>
+
+<style scoped lang="less">
+
+h1 {
+    margin-top: 70px;
+}
+
+</style>
